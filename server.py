a02d1cc95062a7e961bc6ec638ce4bb50c109647
Evgeny Timofeyev <nawasington@ya.ru>
2018-04-16 18:40:16 +0300

    api

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, abort
+import typograf as t
 
 app = Flask(__name__)
 
@@ -6,6 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def form():
     return render_template('form.html')
+
+
+@app.route('/api/v1.0/perform', methods=["POST"])
+def perform():
+    if not request.json or 'text' not in request.json:
+        abort(400)
+    return jsonify({'result': t.perform(request.json['text'])}), 200
 
 
 if __name__ == "__main__":
---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+let form = document.querySelector('form'),
+  resultTextarea = document.getElementById('perform-result');
+
+form.onsubmit = function () {
+  $.post(
+    '/api/v1.0/perform',
+    {
+      text: this.elements.text.value
+    }
+  )
+    .success(function (result) {
+      console.log(result);
+      resultTextarea.value = result
+    })
+    .error(function (message) {
+      console.log(message)
+    });
+  return false;
+};
