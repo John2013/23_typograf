7b798c74e2aae2107c0935cada05b1949c69591d
Evgeny Timofeyev <nawasington@ya.ru>
2018-04-16 19:39:55 +0300

    эксперементирую с аякс запросом

--- static/js/main.js
+++ static/js/main.js
@@ -2,10 +2,13 @@ let form = document.querySelector('form'),
   resultTextarea = document.getElementById('perform-result');
 
 form.onsubmit = function () {
-  $.post(
-    '/api/v1.0/perform',
-    {text: this.elements.text.value}
-  )
+  $.ajax({
+    url: '/api/v1.0/perform',
+    type: "POST",
+    data: {text: form.elements.text.value},
+    contentType: "application/json; charset=utf-8",
+    dataType: "json"
+  })
     .success(function (result) {
       console.log(result);
       resultTextarea.value = result
