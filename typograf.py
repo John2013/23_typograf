1867b645c13fd5c30f5c22333b21d8f5293f6981
Evgeny Timofeyev <nawasington@ya.ru>
2018-12-06 21:05:06 +0300

    сложные однострочные регулярные выражения заменены многострочными с режимом verbose

--- typograf.py
+++ typograf.py
@@ -1,11 +1,19 @@
-from re import sub
+import re
 
 
 def convert_quotes(text):
     in_tag, double, single = 1, 2, 3
 
-    return sub(
-        r'(<(?:"[^"]*"|\'[^\']*\'|[^\'">])*>)|"([^"]*)"|\'([^\']*)\'',
+    quote_finder_re = re.compile(
+        r"""
+        (<(?:"[^"]*"|\'[^\']*\'|[^\'">])*>)  # <tag attr="">
+        |"([^"]*)"                           # ""
+        |\'([^\']*)\'                        # ''
+        """,
+        re.X
+    )
+    return re.sub(
+        quote_finder_re,
         lambda entry:
         "{}".format(entry.group(in_tag)) if entry.group(in_tag) else
             "«{}»".format(entry.group(double) or (entry.group(single) or '')),
@@ -14,30 +22,50 @@ def convert_quotes(text):
 
 
 def convert_hyphen_to_dash(text):
-    return sub(r'\s-\s', ' \u2013 ', text)
+    return re.sub(r'\s-\s', ' \u2013 ', text)
 
 
 def fix_dashes_in_phones(text):
-    return sub(
-        r'((?:(?:\+7)|(?:8))\s?\(?\d{3}\)?\s?\d{3})'
-        r'(?:\s?[-\s\u2013]\s?)(\d{2})'
-        r'(?:\s?[-\s\u2013]\s?)(\d{2})',
+    phone_re = re.compile(
+        r"""
+        (
+            (?:(?:\+7)|(?:8))\s?     # +7 | 8
+            \(?\d{3}\)?\s?           # (999)
+            \d{3}                    # 999
+        )
+        (?:\s?[-\s\u2013]\s?)        # -
+        (\d{2})                      # 99
+        (?:\s?[-\s\u2013]\s?)        # -
+        (\d{2})                      # 99
+        """,
+        re.X
+    )
+    return re.sub(
+        phone_re,
         r'\1{0}\2{0}\3'.format('\u2011'),
         text
     )
 
 
 def put_non_breaking_space_after_numbers(text):
-    return sub(r'(?<=[\u0020\u00a0])(\d+)([\u0020\u00a0]+)', '\\1\u00A0', text)
+    space_after_numbers_re = re.compile(
+        r"""
+        (?<=[\u0020\u00a0]) # space
+        (\d+)               # number
+        ([\u0020\u00a0]+)   # spaces
+        """,
+        re.X
+    )
+    return re.sub(space_after_numbers_re, '\\1\u00A0', text)
 
 
 def remove_extra_space(text):
-    text = sub(r'[\t\f\v ]+', ' ', text)
-    return sub(r'[\r\n]+', '\n', text)
+    text = re.sub(r'[\t\f\v ]+', ' ', text)
+    return re.sub(r'[\r\n]+', '\n', text)
 
 
 def link_conjunctions_with_words(text):
-    return sub(r'(?:\s+([\wА-Яа-я]{1,2})\s+)', ' \\1\u00A0', text)
+    return re.sub(r'(?:\s+([\wА-Яа-я]{1,2})\s+)', ' \\1\u00A0', text)
 
 
 def perform(text):
